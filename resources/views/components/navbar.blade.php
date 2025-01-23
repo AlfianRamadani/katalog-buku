d3d6c602c37f8f6c821f29709a69657bf3c4dda1
meka <seraaptr@gmail.com>
2025-01-23 18:23:30 +0800

    buat navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,34 @@
-<div>
-    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+<nav class="navbar">
+    <div class="container">
+        <!-- Logo -->
+        <div class="navbar-brand">
+    <a href="/" class="brand-name">
+        <span class="brand-my">My</span><span class="brand-cabook">Cabook</span>
+    </a>
 </div>
+
+
+        <!-- Menu Utama -->
+        <div class="navbar-menu">
+            <a href="/" class="nav-item">Home</a>
+            <a href="/request-buku" class="nav-item">Request Buku</a>
+            <a href="/about-us" class="nav-item">About Us</a>
+            <a href="/contact" class="nav-item">Contact</a>
+        </div>
+
+        <!-- Burger Menu (untuk mobile) -->
+        <div class="burger-menu" id="burger-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+    <!-- Mobile Dropdown Menu -->
+    <div class="mobile-menu" id="mobile-menu">
+        <a href="/" class="mobile-item">Home</a>
+        <a href="/request-buku" class="mobile-item">Request Buku</a>
+        <a href="/about-us" class="mobile-item">About Us</a>
+        <a href="/contact" class="mobile-item">Contact</a>
+    </div>
+</nav>
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Halaman Utama</title>
+
+    <!-- Tambahkan link CSS di sini -->
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}"> <!-- CSS lokal -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"> <!-- Contoh CSS eksternal -->
 </head>
 
 <body>
@@ -13,3 +17,4 @@
 </body>
 
 </html>
+
