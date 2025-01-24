125e885b4806ad0b96a10fb0f0e6e2f15ab58c6f
Alfian Ramadani <Alfianr792@gmail.com>
2025-01-24 18:31:29 +0800

    perbaiki tailwindcss config

--- resources/views/home/partial/search.blade.php
+++ resources/views/home/partial/search.blade.php
@@ -1,16 +1,20 @@
-<div class="search-container">
-    <div class="search-header">
+<div class="search-container min-h-[50vh] flex gap-4 flex-col">
+    <div class="search-header flex gap-4">
         <h1>Temukan informasi menarik tentang buku hanya di MyCabook!</h1>
-        <div class="search-input-block">
-            <input name="search" type="text" class="search-input">
-            <x-lucide-search  />
+        <div class="relative w-1/3 divide-x-2 divide-black">
+            <input name="search" type="text"
+                class="w-full border-2 border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                placeholder="Cari buku...">
+            <div class="absolute top-1/2 transform -translate-y-1/2 right-3 ">
+                <x-lucide-search class="w-6 h-6 text-gray-500" />
+            </div>
         </div>
     </div>
-    <div class="search-links">
-        <a href="" class="search-link">Novel</a>
-        <a href="" class="search-link">Cerpen</a>
-        <a href="" class="search-link">Sejarah</a>
-        <a href="" class="search-link">Komik</a>
-        <a href="" class="search-link">Manga</a>
+    <div class="flex gap-4 mt-4 justify-center text-center items-center">
+        <a class="min-w-20 h-6  bg-green-500 text-white rounded-lg hover:bg-green-600 transition" href="">Novel</a>
+        <a class="min-w-20 h-6 bg-green-500 text-white rounded-lg hover:bg-green-600 transition" href="">Cerpen</a>
+        <a class="min-w-20 h-6 bg-green-500 text-white rounded-lg hover:bg-green-600 transition" href="">Sejarah</a>
+        <a class="min-w-20 h-6 bg-green-500 text-white rounded-lg hover:bg-green-600 transition" href="">Komik</a>
+        <a class="min-w-20 h-6 bg-green-500 text-white rounded-lg hover:bg-green-600 transition" href="">Manga</a>
     </div>
 </div>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Laravel App')</title>
-    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}"> <!-- Tambahkan CSS -->
-    <script src="{{ asset('js/navbar.js') }}"></script>
-
 </head>
 
 <body>
