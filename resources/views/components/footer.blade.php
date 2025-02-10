d7e02a4d2f2ca7512d4519beb55f2ad9e0d582f6
Alfian Ramadani <Alfianr792@gmail.com>
2025-02-11 07:58:02 +0800

    change footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -13,14 +13,14 @@
         <!-- Menu Navigasi -->
         <div class="footer-menu">
             <a href="/" class="footer-link">Home</a>
-            <a href="/request-buku" class="footer-link">Request Buku</a>
-            <a href="/about-us" class="footer-link">About Us</a>
-            <a href="/contact" class="footer-link">Contact</a>
+            <a href="/about-us" class="footer-link">Tentang Kami</a>
+            <a href="/contact" class="footer-link">Kontak</a>
         </div>
     </div>
 
     <!-- Copyright -->
     <div class="footer-bottom">~
-        <p>&copy; {{ date('Y') }} <span>My <span class="brand-cabook">Cabook</span></span>. All Rights Reserved .</p>
+        <p>&copy; {{ date('Y') }} <span>My <span class="brand-cabook">Cabook</span></span>. All Rights Reserved .
+        </p>
     </div>
 </footer>
