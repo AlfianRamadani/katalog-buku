e37f9d864e780b297f9c16d032ce1bf19eb08eb3
nrmdhan2007 <naufalramadhan2607@gmail.com>
2025-02-17 10:35:25 +0800

    fungsi contact us

---
+++ app/Filament/Resources/ContactUsResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContactUsResource\Pages;
+use App\Filament\Resources\ContactUsResource\RelationManagers;
+use App\Models\ContactUs;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ContactUsResource extends Resource
+{
+    protected static ?string $model = ContactUs::class;
+
+    protected static ?string $navigationLabel = 'Hubungi Kami
+    
+    
+    ';
+    public static ?string $pluralModelLabel = ' Hubungi Kami';
+    protected static ?string $navigationIcon = 'heroicon-o-chat-bubble-left-right';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                ->label("Nama Pengirim")
+                ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                ->label("Email Pengirim")
+                ->searchable(),
+                Tables\Columns\TextColumn::make('message')
+                ->label("Pesan Pengirim")
+                ->searchable(),
+            ])
+            
+            ->filters([
+                //
+            ])
+            ->actions([
+               
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContactUs::route('/'),
+            'create' => Pages\CreateContactUs::route('/create'),
+            'edit' => Pages\EditContactUs::route('/{record}/edit'),
+        ];
+    }
+}
